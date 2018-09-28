c0c3a3e020b70434b3613b581b858f48bf3ebbb6
Nurbek <nurbekizbassar@gmail.com>
2018-09-29 00:53:17 +0600

    registration and login templates with tons of validations

---
+++ dang-thats-delicios/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+mongoose.Promise = global.Promise;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: 'email жалғыз болу керек',
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid email address'],
+    required: 'Please supply an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please suplly a name',
+    trim: true
+  }
+})
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
